97010838cfce424c5a844b13086b20eb0d1f332a
Alex <Alexkeen2003@gmail.com>
2025-02-21 21:44:46 -0600

    python files

---
+++ src/delete.py
@@ -0,0 +1,106 @@
+import numpy as np
+
+fivebyfivesolver = np.array([
+  [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+   0, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+   0, 0, 0],
+  [0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 
+   0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 
+   0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 
+   0, 0, 0],
+  [0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 
+   0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 
+   0, 0, 0],
+  [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 
+   0, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 
+   0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 
+   0, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 
+   0, 0, 0],
+  [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 
+   1, 0, 0],
+  [0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 
+   0, 1, 0],
+  [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 
+   1, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 
+   1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+   1, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 
+   0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 
+   0, 0, 1],
+  [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 
+   0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 
+   1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 
+   0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 
+   0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 
+   1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 
+   0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 
+   0, 0, 1]
+ ])
+
+matrix = np.array([[1, 1, 0, 0, 0],
+                   [0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 1]])
+
+matrix_to_column = matrix.flatten().reshape(-1, 1)
+
+# Apply modulus 2 to both the solver matrix and the column vector
+modulus = 2
+fivebyfivesolver_mod2 = fivebyfivesolver % modulus
+matrix_to_column_mod2 = matrix_to_column % modulus
+
+
+# Function to perform Gaussian elimination mod 2
+def gaussian_elimination_mod2(A, b):
+    n = len(b)
+
+    # Augment matrix A with column b (right-hand side)
+    augmented = np.hstack([A, b])
+
+    # Perform Gaussian elimination
+    for i in range(n):
+        # Make the diagonal element 1 (pivoting step, if necessary)
+        if augmented[i, i] == 0:
+            # Try to swap rows if necessary
+            for j in range(i + 1, n):
+                if augmented[j, i] == 1:
+                    augmented[[i, j]] = augmented[[j, i]]
+                    break
+
+        # Eliminate all below the pivot
+        for j in range(i + 1, n):
+            if augmented[j, i] == 1:
+                augmented[j] ^= augmented[i]
+
+    # Back-substitution to find solution
+    solution = np.zeros(n, dtype=int)
+    for i in range(n - 1, -1, -1):
+        solution[i] = augmented[i, -1] - np.dot(augmented[i, :-1], solution) % modulus
+
+    return solution
+
+
+# Solve using Gaussian elimination mod 2
+solution = gaussian_elimination_mod2(fivebyfivesolver_mod2, matrix_to_column_mod2)
+print("Solution mod 2:", solution)
+sol = []
+for r in solution:
+    sol.append(int(r))
+print(sol)
